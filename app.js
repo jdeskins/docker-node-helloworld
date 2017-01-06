ba081c95b1ef8798dd8e38e8b59621f6f7c6efcb
Jeff Deskins <Jeff.Deskins@lifeway.com>
2017-01-06 15:53:22 -0600

    cleanup

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.get('/version', function(req, res) {
 //});
 
 
-
 var server = app.listen(process.env.PORT || 8080, function () {
   var host = server.address().address;
   var port = server.address().port;
